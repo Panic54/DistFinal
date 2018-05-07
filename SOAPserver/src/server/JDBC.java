15ec61751a5c03868c2c4d0927a5f2e676f6e06f
Morten <Morten@DESKTOP-KRMJ2KI.eduroam.wireless.dtu.dk>
2018-05-07 12:52:50 +0200

    Initial commit

---
+++ SOAPserver/src/server/JDBC.java
@@ -0,0 +1,98 @@
+package server;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+import javax.jws.WebService;
+
+import server.DTO;
+import server.JDBCI;
+
+@WebService(endpointInterface = "server.JDBCI")
+public class JDBC implements JDBCI {
+
+	Connection myConn;
+	Statement statement;
+	ResultSet res;
+
+	JDBC() {
+
+		try {
+
+			myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/gruppe50", "root", "");
+			statement = myConn.createStatement();
+
+			// res = statement.executeQuery("Some Query");
+			// while(res.next()){
+			// System.out.println(res.getString("user") + ", " + res.getString("item") + ",
+			// " + res.getString("price"));
+			// }
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public ArrayList<DTO> getTable() {
+
+		ArrayList<DTO> list = new ArrayList<DTO>();
+
+		try {
+			res = statement.executeQuery("SELECT * FROM items");
+
+			while (res.next()) {
+				DTO dto = new DTO();
+				dto.setName(res.getString("user"));
+				dto.setItem(res.getString("item"));
+				dto.setPrice(res.getInt("price"));
+				list.add(dto);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return list;
+	}
+
+	public void addRow(String name, String item, int price) {
+
+		try {
+			statement.execute("INSERT INTO items VALUES('" + name + "', '" + item + "', " + price + ")");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public void close() {
+		try {
+			res.close();
+			statement.close();
+			if (myConn != null) {
+				myConn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public void delete(String name) {
+		try {
+			statement.execute("DELETE FROM items WHERE user = " + "'" + name + "'");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void deleteItem(String name, String item) {
+		try {
+			statement.execute("DELETE FROM items WHERE user = " + "'" + name + "' AND item =" + "'" + item + "'");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
---
+++ SOAPserver/src/server/JDBCserver.java
@@ -0,0 +1,15 @@
+package server;
+
+import javax.xml.ws.Endpoint;
+
+public class JDBCserver {
+
+	public static void main(String[] args) {
+		System.out.println("publicerer kontotjeneste");
+		JDBCI data = new JDBC();
+		Endpoint.publish("http://ec2-18-188-46-76.us-east-2.compute.amazonaws.com:11111/JDBCxml?wsdl", data);
+		System.out.println("JDBC server er startet.");
+
+	}
+
+}
